d5b03a15adeeaedebf6973b05fb303033882d49b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-14 16:01:30 +0100

    Add tests (#3)

---
+++ include/Ishiko/JSON.hpp
@@ -0,0 +1,12 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/json/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_JSON_HPP_
+#define _ISHIKO_CPP_JSON_HPP_
+
+#include "JSON/JSONPushParser.hpp"
+
+#endif
---
+++ tests/src/main.cpp
@@ -0,0 +1,20 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/errors/blob/main/LICENSE.txt
+*/
+
+#include "JSONPushParserTests.hpp"
+#include <Ishiko/TestFramework/Core.hpp>
+
+using namespace Ishiko;
+
+int main(int argc, char* argv[])
+{
+    TestHarness theTestHarness("IshikoJSON");
+
+    TestSequence& theTests = theTestHarness.tests();
+    theTests.append<JSONPushParserTests>();
+
+    return theTestHarness.run();
+}
