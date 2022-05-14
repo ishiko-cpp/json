a69dedce198054b03f5308595f0e2539f70e14db
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-14 14:19:04 +0100

    Add test file

---
+++ tests/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/errors/blob/main/LICENSE.txt
+*/
+
+#include <Ishiko/TestFramework/Core.hpp>
+
+using namespace Ishiko;
+
+int main(int argc, char* argv[])
+{
+    TestHarness theTestHarness("IshikoErrors");
+
+    return theTestHarness.run();
+}
