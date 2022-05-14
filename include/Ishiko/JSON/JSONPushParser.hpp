8c48749bcf6b423ac73c3d38d10a477825dd1661
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-14 14:08:59 +0100

    Add files

---
+++ include/Ishiko/JSON/JSONPushParser.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/json/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_JSON_JSONPUSHPARSER_HPP_
+#define _ISHIKO_CPP_JSON_JSONPUSHPARSER_HPP_
+
+#endif
