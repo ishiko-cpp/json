acf61382ea6166a697a1b8948910212ef3ebe6c6
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-24 18:08:41 +0000

    Updates

--- include/Ishiko/JSON.hpp
+++ include/Ishiko/JSON.hpp
@@ -1,11 +1,8 @@
-/*
-    Copyright (c) 2022 Xavier Leclercq
-    Released under the MIT License
-    See https://github.com/ishiko-cpp/json/blob/main/LICENSE.txt
-*/
+// SPDX-FileCopyrightText: 2005-2024 Xavier Leclercq
+// SPDX-License-Identifier: BSL-1.0
 
-#ifndef _ISHIKO_CPP_JSON_HPP_
-#define _ISHIKO_CPP_JSON_HPP_
+#ifndef GUARD_ISHIKO_CPP_JSON_HPP
+#define GUARD_ISHIKO_CPP_JSON_HPP
 
 #include "JSON/JSONPushParser.hpp"
 #include "JSON/linkoptions.hpp"
--- include/Ishiko/JSON/JSONPushParser.hpp
+++ include/Ishiko/JSON/JSONPushParser.hpp
@@ -1,75 +1,70 @@
-/*
-    Copyright (c) 2022 Xavier Leclercq
-    Released under the MIT License
-    See https://github.com/ishiko-cpp/json/blob/main/LICENSE.txt
-*/
+// SPDX-FileCopyrightText: 2005-2024 Xavier Leclercq
+// SPDX-License-Identifier: BSL-1.0
 
-#ifndef _ISHIKO_CPP_JSON_JSONPUSHPARSER_HPP_
-#define _ISHIKO_CPP_JSON_JSONPUSHPARSER_HPP_
+#ifndef GUARD_ISHIKO_CPP_JSON_JSONPUSHPARSER_HPP
+#define GUARD_ISHIKO_CPP_JSON_JSONPUSHPARSER_HPP
 
 #include <boost/utility/string_view.hpp>
 #include <vector>
 
 namespace Ishiko
 {
-
-class JSONPushParser
-{
-public:
-    class Callbacks
+    class JSONPushParser
     {
     public:
-        virtual ~Callbacks() = default;
+        class Callbacks
+        {
+        public:
+            virtual ~Callbacks() = default;
 
-        virtual void onObjectBegin();
-        virtual void onMemberBegin();
-        virtual void onMemberName(boost::string_view data);
-        virtual void onMemberElementBegin();
-        virtual void onMemberElementEnd();
-        virtual void onMemberEnd();
-        virtual void onObjectEnd();
-        virtual void onArrayBegin();
-        virtual void onArrayEnd();
-        virtual void onString(boost::string_view data);
-        virtual void onTrue();
-        virtual void onFalse();
-        virtual void onNull();
-        virtual void onWhitespace(boost::string_view data);
+            virtual void onObjectBegin();
+            virtual void onMemberBegin();
+            virtual void onMemberName(boost::string_view data);
+            virtual void onMemberElementBegin();
+            virtual void onMemberElementEnd();
+            virtual void onMemberEnd();
+            virtual void onObjectEnd();
+            virtual void onArrayBegin();
+            virtual void onArrayEnd();
+            virtual void onString(boost::string_view data);
+            virtual void onTrue();
+            virtual void onFalse();
+            virtual void onNull();
+            virtual void onWhitespace(boost::string_view data);
+        };
+
+        JSONPushParser(Callbacks& callbacks);
+
+        bool onData(boost::string_view data, bool eod);
+
+    private:
+        enum class ParsingMode
+        {
+            json,
+            objectWs1,
+            objectMemberOrRightCurlyBracket,
+            objectMemberName,
+            objectWs2,
+            objectColon,
+            objectElement,
+            objectCommaOrRightCurlyBracket,
+            arrayWs1,
+            arrayElementOrRightSquareBracket,
+            arrayCommaOrRightSquareBracket,
+            valueString,
+            valueTrue,
+            valueFalse,
+            valueNull,
+            elementWs1,
+            elementValue,
+            elementWs2,
+            ws
+        };
+
+        std::vector<ParsingMode> m_parsingModeStack;
+        std::string m_fragmentedData;
+        Callbacks& m_callbacks;
     };
-
-    JSONPushParser(Callbacks& callbacks);
-
-    bool onData(boost::string_view data, bool eod);
-
-private:
-    enum class ParsingMode
-    {
-        json,
-        objectWs1,
-        objectMemberOrRightCurlyBracket,
-        objectMemberName,
-        objectWs2,
-        objectColon,
-        objectElement,
-        objectCommaOrRightCurlyBracket,
-        arrayWs1,
-        arrayElementOrRightSquareBracket,
-        arrayCommaOrRightSquareBracket,
-        valueString,
-        valueTrue,
-        valueFalse,
-        valueNull,
-        elementWs1,
-        elementValue,
-        elementWs2,
-        ws
-    };
-
-    std::vector<ParsingMode> m_parsingModeStack;
-    std::string m_fragmentedData;
-    Callbacks& m_callbacks;
-};
-
 }
 
 #endif
